c007c8282af9487b1d1b2b45a6e8cd69ededa68e
melanytorrez <torrezmelany13@gmail.com>
2025-02-20 09:58:03 -0400

    feat:genera buzz si el numero es multiplo de 5

--- src/fizzbuzz.spec.js
+++ src/fizzbuzz.spec.js
@@ -11,11 +11,11 @@ describe("fizzbuzz", () =>{
     it("deberia generar Fizz si el numero es divisible por 3",()=>{
         expect(fizzbuzz(3)).toEqual('Fizz');
       });
-/*
+
       it("deberia generar Buzz si el numero es divisible por 5",()=>{
         expect(fizzbuzz(5)).toEqual('Buzz');
       });
-
+/*
       it("deberia generar FizzBuzz si el numero es divisible por 3 y 5",()=>{
         expect(fizzbuzz(15)).toEqual('FizzBuzz');
       });

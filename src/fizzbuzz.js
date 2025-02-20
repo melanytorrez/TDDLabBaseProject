007fb8eafeca5f28c3dd13532ccbca07cda9416f
melanytorrez <torrezmelany13@gmail.com>
2025-02-20 09:54:33 -0400

    si el numero es multiplo de 3 retorna fizz

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,5 +1,8 @@
 function fizzbuzz(num){
-    return num + " ";
+    if(num % 3 === 0){
+        return "Fizz";
+    }
+    return num + "";
 }
 
 export default fizzbuzz;
--- src/fizzbuzz.spec.js
+++ src/fizzbuzz.spec.js
@@ -7,11 +7,11 @@ describe("fizzbuzz", () =>{
     it("deberia generar el mismo numero si no sigue una regla",()=>{
         expect(fizzbuzz(2)).toEqual('2');
       });
-/*
+
     it("deberia generar Fizz si el numero es divisible por 3",()=>{
         expect(fizzbuzz(3)).toEqual('Fizz');
       });
-
+/*
       it("deberia generar Buzz si el numero es divisible por 5",()=>{
         expect(fizzbuzz(5)).toEqual('Buzz');
       });
